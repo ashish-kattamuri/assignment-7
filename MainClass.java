cc3698ca9df4202c4c601c30d8927feedcc5fced
ashish-kattamuri <69315739+ashish-kattamuri@users.noreply.github.com>
2022-01-23 12:54:58 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,66 +1,40 @@
-import java.util.Scanner;
-interface A{
-	public void getA();
-	public void dispA();
-}
-interface B{
-	public void getB();
-	public void dispB();
-}
-interface C{
-	public void getC();
-	public void dispC();
+interface Cycle{
+	void code();
+	void factories();
 
 }
-interface D implements A,B,C{
-	public void getD();
+class Unicycle implements Cycle{
+	public void code(){
+		factories();
+	}
+	public void factories(){
+		System.out.println("Code of Unicycle is U_cyc.");
+	}
 
-}
-class E implements D{
-	public void getA(){
-		System.out.println("interface A get method");
+};
+class Bicycle implements Cycle{
+	public void code(){
+		factories();
 	}
-	public void dispA(){
-		System.out.println("interface A disp method");
-	}
-	public void getB(){
-		System.out.println("interface B get method");
-	}
-	public void dispB(){
-		System.out.println("interface B disp method");
-	}
-	public void getC(){
-		System.out.println("interface C get method");
-	}
-	public void dispC(){
-		System.out.println("interface C disp method");
-	}
-	public void getD(){
-		System.out.println("interface D get mehtod ");
+	public void factories(){
+		System.out.println("Code of Bicycle is B_cyc.");
 	}
 };
-
+class Tricycle implements Cycle{
+	public void code(){
+		factories();
+	}
+	public void factories(){
+		System.out.println("Code of tricycle is T_cyc.");
+	}
+};
 class MainClass {
-
-	static void method1(Interface1 i) {
-	};
-	static void method2(Interface2 i) {
-	};
-	static void method3(Interface3 i) {
-	};
-	static void method4(Newinterface i) {
-	};
-
-	public static void main(String[] args) {
-		E s = new E();
-		String st;
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the name of the interface");
-		st = in.nextLine();
-		s.method1(st);
-		s.method2(st);
-		s.method3(st);
-		s.method4(st);
-		
+	public static void main(String[] args){
+		Unicycle obj = new Unicycle();
+		obj.code();
+		Bicycle obj1 = new Bicycle();
+		obj1.code();
+		Tricycle obj2 = new Tricycle();
+		obj2.code();
 	}
 }
