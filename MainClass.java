28acb6cdef73d8ecacba51e8f6e4153c5f140415
ashish-kattamuri <69315739+ashish-kattamuri@users.noreply.github.com>
2022-01-23 12:54:26 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,27 +1,66 @@
-abstract class Cycle{
-	public abstract void balance();
-};
-class Unicycle extends Cycle{
-	public void balance(){
-		System.out.println("The cost of Unicycle start with RS 2500. ");
+import java.util.Scanner;
+interface A{
+	public void getA();
+	public void dispA();
+}
+interface B{
+	public void getB();
+	public void dispB();
+}
+interface C{
+	public void getC();
+	public void dispC();
+
+}
+interface D implements A,B,C{
+	public void getD();
+
+}
+class E implements D{
+	public void getA(){
+		System.out.println("interface A get method");
+	}
+	public void dispA(){
+		System.out.println("interface A disp method");
+	}
+	public void getB(){
+		System.out.println("interface B get method");
+	}
+	public void dispB(){
+		System.out.println("interface B disp method");
+	}
+	public void getC(){
+		System.out.println("interface C get method");
+	}
+	public void dispC(){
+		System.out.println("interface C disp method");
+	}
+	public void getD(){
+		System.out.println("interface D get mehtod ");
 	}
 };
-class Bicycle extends Cycle{
-	public void balance(){
-		System.out.println("The cost of Bicycle start with Rs 3000. ");
-	}
-};
-class Tricycle extends Cycle{
-//getting error in this code as Tricycle class inherite class Cycle but balance() is not declare by this class
-};
-public class MainClass{
+
+class MainClass {
+
+	static void method1(Interface1 i) {
+	};
+	static void method2(Interface2 i) {
+	};
+	static void method3(Interface3 i) {
+	};
+	static void method4(Newinterface i) {
+	};
+
 	public static void main(String[] args) {
-		Cycle[] obj = new Cycle[3];
-		obj[0] = new Unicycle();
-		obj[0].balance();	
-		obj[1] = new Bicycle();
-		obj[1].balance();	
-		obj[2] = new Tricycle();
-		obj[2].balance();
+		E s = new E();
+		String st;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the name of the interface");
+		st = in.nextLine();
+		s.method1(st);
+		s.method2(st);
+		s.method3(st);
+		s.method4(st);
+		
 	}
 }
